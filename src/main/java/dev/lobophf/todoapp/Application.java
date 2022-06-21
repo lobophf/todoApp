6516957fd1c338130cbb972ab54ab0840b01472c
Pedro Lobo <lobophf@gmail.com>
2022-06-20 21:26:36 -0300

    connecting with database

--- src/main/java/dev/lobophf/todoapp/Application.java
+++ src/main/java/dev/lobophf/todoapp/Application.java
@@ -1,9 +1,17 @@
 package dev.lobophf.todoapp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Application {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
     System.out.println("Hi");
-  }
 
+    Class.forName("com.mysql.cj.jdbc.Driver");
+
+    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/todoApp", "todoAppUser", "secret");
+    con.close();
+  }
 }
